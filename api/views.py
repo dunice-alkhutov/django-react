f5482fde307d5c6f493f63ea9583fe9e1d479c64
Aleksey Alkhutov <alexey.alkhutov@dunice.ru>
2017-04-11 17:07:37 +0300

    --

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, Board
+from .serializers import BookSerializer, BoardSerializer 
 
 class BookList(generics.ListCreateAPIView):
 	"""
@@ -9,3 +9,11 @@ class BookList(generics.ListCreateAPIView):
 	"""
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
+
+
+class BoardList(generics.ListCreateAPIView):
+	"""
+	API endpoint for listing and creating Board objects
+	"""
+	queryset = Board.objects.all()
+	serializer_class = BoardSerializer
